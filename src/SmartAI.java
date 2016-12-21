8f27445ef710d4910fa90cf8b60390037c20fb0e
Daniel Stephenson <dmccoystephenson@gmail.com>
2016-12-20 23:15:40 -0700

    Planned out class

--- src/SmartAI.java
+++ src/SmartAI.java
@@ -1,4 +1,35 @@
 
 public class SmartAI {
-
+	private static StringGrid grid;
+	private static String selection;
+	
+	public SmartAI(StringGrid g, String s) {
+		grid = g;
+		selection = s;
+	}
+	
+	public static void checkIfEnemyAboutToWin() {
+		// checks if two of opposite move in a row, and if so, block them
+	}
+	
+	public static boolean middleTaken() {
+		// checks if middle position is taken
+		
+		return false; //change
+	}
+	
+	public static void takeMiddle() {
+		// takes middle position
+	}
+	
+	public static boolean cornerTaken() {
+		// do I need this?
+		
+		return false; //change
+	}
+	
+	public static void takeCorner() {
+		// take corner depending on which one is most viable option
+	}
+	
 }
